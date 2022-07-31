017b1e65a7aa567451c8441998fb73c41ccc2cb5
yazawayuma1203 <110238678+yazawayuma1203@users.noreply.github.com>
2022-07-31 18:45:02 +0900

    useEffect useEffectの使い方

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,29 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import ColorfulMessage from "./components/ColorfulMessage";
 
 const App = () => {
+  console.log("最初");
+  const [num, setNum] = useState(0);
+  const [faceShowFlag, setfaceShowFlag] = useState(false);
+
   const onClickCountUp = () => {
     setNum(num + 1);
   };
-  const [num, setNum] = useState(0);
+
+  const onClickSwitchShowFlag = () => {
+    setfaceShowFlag(!faceShowFlag);
+  };
+
+  useEffect(() => {
+    if (num > 0) {
+      if (num % 3 === 0) {
+        faceShowFlag || setfaceShowFlag(true);
+      } else {
+        faceShowFlag && setfaceShowFlag(false);
+      }
+    }
+    // eslint-disable-next-line
+  }, [num]);
 
   return (
     <>
@@ -14,6 +32,8 @@ const App = () => {
       <ColorfulMessage color="pink">元気です!</ColorfulMessage>
       <button onClick={onClickCountUp}>カウントアップ</button>
       <p>{num}</p>
+      <button onClick={onClickSwitchShowFlag}>on/off</button>
+      {faceShowFlag && <p>( *ﾟ∀ﾟ)━━);ﾟ;Д;ﾟ;;)・;'.、━σ ﾄﾞｷﾞｭｰﾝ!!</p>}
     </>
   );
 };
