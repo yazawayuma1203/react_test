313da22c731ed90e331d56acb027fc2db490e222
yazawayuma1203 <110238678+yazawayuma1203@users.noreply.github.com>
2022-07-31 17:41:06 +0900

    コンポーネント化 コンポーネント化まで

--- src/index.js
+++ src/index.js
@@ -1,11 +1,5 @@
-import { StrictMode } from "react";
 import ReactDom from "react-dom";
-
+import React from "react";
 import App from "./App";
 
-ReactDom.render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-  document.getElementById("root")
-);
+ReactDom.render(<App />, document.getElementById("root"));
